d81415019ab675400e37fc5f4b206a2157f4c1bf
antoniopco <antoniopco95@email.com>
2023-06-24 18:03:24 -0300

    data e dia da semana quase finalizados

--- dindin/src/components/TableLine/index.jsx
+++ dindin/src/components/TableLine/index.jsx
@@ -7,6 +7,23 @@ import { getItem } from "../../utils/localStorage";
 import { format } from "date-fns";
 
 function TableLine({ listTransaction }) {
+  function chooseDay(data) {
+    if (new Date().getDay(data) === 0) {
+      return "Segunda";
+    } else if (new Date(data).getDay() === 1) {
+      return "Terça";
+    } else if (new Date(data).getDay() === 2) {
+      return "Quarta";
+    } else if (new Date(data).getDay() === 3) {
+      return "Quinta";
+    } else if (new Date(data).getDay() === 4) {
+      return "Sexta";
+    } else if (new Date(data).getDay() === 5) {
+      return "Sábado";
+    } else if (new Date(data).getDay() === 6) {
+      return "Domingo";
+    }
+  }
   return (
     <>
       {listTransaction.map((item) => (
@@ -14,7 +31,7 @@ function TableLine({ listTransaction }) {
           <span className="line1">
             {format(new Date(item.data), "dd/MM/yyy")}
           </span>
-          <span className="line2">Sexta</span>
+          <span className="line2">{chooseDay(item.data)}</span>
           <span className="line3">{item.categoria_nome}</span>
           <span className="line4">{item.descricao}</span>
           <span className="line5">R$ {item.valor}</span>
