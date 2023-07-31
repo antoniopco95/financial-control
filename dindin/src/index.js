5c170aa98ef3054bf13906a883ef75cf86352e24
antoniopco95 <antoniopco95@gmail.com>
2023-07-31 03:23:19 -0300

    vercel-analytics

--- dindin/src/index.js
+++ dindin/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import MyRoutes from "./routes";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from '@vercel/analytics';
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
